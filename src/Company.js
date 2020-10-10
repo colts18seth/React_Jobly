abb725d090036aa058c2dbbe03702e5b35c8375d
Seth <colts18seth@gmail.com>
2020-10-09 21:20:49 -0400

    fixed getcompany api call in company component

--- src/Company.js
+++ src/Company.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import {v4 as uuid} from 'uuid';
 import { useParams } from 'react-router-dom';
 import Api from './Api';
+import './Company.css';
 
 function Company() {
     const { company } = useParams();
-    const [companyState, setCompanyState] = useState();
+    const [companyState, setCompanyState] = useState("");
 
     useEffect(() => {
         const gotCompany = Api.getCompany(company);
@@ -15,7 +17,20 @@ function Company() {
 
     return (
         <div className="Company">
-            
+            <h3>{companyState.name}</h3>
+            <p>{companyState.description}</p>
+            {companyState !== "" && 
+            <div>{companyState.jobs.map(job => (
+                <div className='job' key={uuid()}>
+                    <h4>{job.title}</h4>
+                        <p>Salary: {job.salary}</p>
+                        <p>Equity: {job.equity}</p>
+
+                        <button>Apply</button>
+                </div>
+            ))}
+            </div>     
+            }       
         </div>
     );
 }
