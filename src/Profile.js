93de8c58455b40f238efea99ec098a3dffe1dc72
Seth <colts18seth@gmail.com>
2020-10-14 20:44:26 -0400

    added profile info

--- src/Profile.js
+++ src/Profile.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import './Profile.css';
 
 function Profile({currentUser}) {
     const user = currentUser;
+
+    //? add state for showing form for updating user
+
     return (
         <div className="Profile">
             {user && 
+            <>
                 <h1>{user.first_name} {user.last_name}</h1>
+                <p>Username: {user.username}</p>
+                <p>Email: {user.email}</p>
+                <p>Photo URL: {user.photo_url ? user.photo_url : "None"}</p>
+                <button>Update Info</button>
+            </>
             }
+
+            {/* //? add form for user update */}
+
         </div>
     );
 }
